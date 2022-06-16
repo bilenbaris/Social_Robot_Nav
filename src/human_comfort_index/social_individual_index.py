dd4e538fee25173048471de70fe3a4200fad40c6
bilenbaris <bilenbaris97@gmail.com>
2022-06-17 00:50:40 +0300

    bug fixes

--- src/human_comfort_index/social_individual_index.py
+++ src/human_comfort_index/social_individual_index.py
@@ -7,66 +7,88 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 from std_msgs.msg import Float32
 import math
 
-# def callback(people, pose):
-#     print("Hello")
-#     people_x = people.people[0].position.x
-#     people_y = people.people[0].position.y
+# def people_callback(people):
+#     control_val = 0.5
+#     control_publisher.publish(control_val)
+#     callback_args = people
 
+#     rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, pose_callback, callback_args)
+
+
+# def pose_callback(pose, args):
 #     pose_x = pose.pose.pose.position.x
 #     pose_y = pose.pose.pose.position.y
 
-#     deneme = math.sqrt((people_x-pose_x)**2+(people_y-pose_y)**2)
-#     distance_publisher.publish(deneme)
+#     print(args)
+#     if(args == None):
+#         people_x = 0
+#         people_y = 0
+#     else:
+#         people_x = args.people[0].pos.x
+#         people_y = args.people[0].pos.y
+    
+#     distance_x = people_x - pose_x
+#     distance_y = people_y - pose_y
+
+#     d_c = 1.5 #Hall's proxemics
+
+#     x = (distance_x / (math.sqrt(2) * d_c))
+#     y = (distance_y / (math.sqrt(2) * d_c))
+#     SII = math.exp(-1 * ((x * x) + (y * y)))
+#     distance_publisher.publish(SII)
+
+
 
 # if __name__ == '__main__':
     
 #     rospy.init_node('social_individual_index')
-#     distance_publisher = rospy.Publisher('sii_distance', Float32, queue_size=10)
+#     distance_publisher = rospy.Publisher('sii_value', Float32, queue_size=10)
+#     control_publisher = rospy.Publisher('sii_control', Float32, queue_size=10)
+    
 
-#     ###---Synchron---###
-#     people_sub = message_filters.Subscriber('/people', People)
-#     pose_sub = message_filters.Subscriber('/amcl_pose', PoseWithCovarianceStamped)
-#     ts = message_filters.ApproximateTimeSynchronizer([people_sub, pose_sub], queue_size=10, slop=1)
-#     ts.registerCallback(callback)
+    
+#     rospy.Subscriber("/leg_tracker_measurements", PositionMeasurementArray, people_callback)
+    
 #     rospy.spin()
 
-#ododm ve baselikn denenmeli çalışığ çaşlışmadığını anlamak için (baselink tf için yenisini buşl)
 
-def people_callback(people):
-    callback_args = people
-    # people_x = people.people[0].pos.x
-    # people_y = people.people[0].pos.y
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////PERSON PUBLISHED/////////////////////////////////////////////////////////////
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    # print("Human")
-    # print(people_x, people_y)
-    rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, pose_callback, callback_args)
+# def people_callback(people):
+    
+#     callback_args = people
+#     rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, pose_callback, callback_args)
 
 
-def pose_callback(pose, args):
+def pose_callback(pose): #, args
+    control_val = 0.5
+    control_publisher.publish(control_val)
     pose_x = pose.pose.pose.position.x
     pose_y = pose.pose.pose.position.y
 
-    people_x = args.people[0].pos.x
-    people_y = args.people[0].pos.y
+    people_x = 97
+    people_y = 97.42
+    
+    distance_x = people_x - pose_x
+    distance_y = people_y - pose_y
 
-    # print("Human")
-    # print(people_x, people_y)
+    d_c = 0.8 #Hall's proxemics
 
-    # print("Robot")
-    # print(pose_x, pose_y)
-
-    distance = math.sqrt((people_x-pose_x)**2+(people_y-pose_y)**2)
-    closeness = 1 / distance
-    # print("Distance:",closeness)
-    distance_publisher.publish(closeness)
+    x = (distance_x / (math.sqrt(2) * d_c))
+    y = (distance_y / (math.sqrt(2) * d_c))
+    SII = math.exp(-1 * ((x * x) + (y * y)))
+    distance_publisher.publish(SII)
 
 
 if __name__ == '__main__':
     
     rospy.init_node('social_individual_index')
-    distance_publisher = rospy.Publisher('sii_distance', Float32, queue_size=10)
-
+    distance_publisher = rospy.Publisher('sii_value', Float32, queue_size=10)
+    control_publisher = rospy.Publisher('sii_control', Float32, queue_size=10)
     
-    rospy.Subscriber("/leg_tracker_measurements", PositionMeasurementArray, people_callback)
+    # rospy.Subscriber("/people", People, people_callback)
+    rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, pose_callback)
     
     rospy.spin()
--- src/people_publisher/personPublisher.py
+++ src/people_publisher/personPublisher.py
@@ -16,12 +16,20 @@ def talker():
     while not rospy.is_shutdown():
 
         #(103,100),(),(),()
-        new_person1 = Person("baris", Point(97, 97.5, 0),Point(0,0,0), 0.0, [],[])
-        # new_person2 = Person("baris2", Point(103, 100, 0),Point(0,0,0), 0.0, [],[])
+        new_person1 = Person("baris", Point(97, 97.42, 0),Point(0,0,0), 0.0, [],[])
+        # new_person2 = Person("baris2", Point(104, 100.2, 0),Point(0,0,0), 0.0, [],[])
+        # new_person3 = Person("baris3", Point(97.3, 104, 0),Point(0,0,0), 0.0, [],[])
+        # new_person4 = Person("baris4", Point(104.3, 106.8, 0),Point(0,0,0), 0.0, [],[])
+        # new_person5 = Person("baris5", Point(97, 105.7, 0),Point(0,0,0), 0.0, [],[])
+
         converted_people1 = People()
         converted_people1.header.frame_id = "map"
         converted_people1.people.append(new_person1)
         # converted_people1.people.append(new_person2)
+        # converted_people1.people.append(new_person3)
+        # converted_people1.people.append(new_person4)
+        # converted_people1.people.append(new_person5)
+
         pub.publish(converted_people1)
 if __name__ == '__main__':
     try:
